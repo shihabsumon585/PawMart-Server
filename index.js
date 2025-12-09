9bd6345e4593eb5825c4cd1636bbb9000ff4f9de
Md Shihab Sumon <shihabsumon585@gmail.com>
2025-12-09 14:05:35 +0600

    Complete backend

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
@@ -10,7 +11,8 @@ app.use(express.json());
 
 // Uxmq0r572YdBK2zT
 
-const uri = "mongodb+srv://SmartDealsServer:Uxmq0r572YdBK2zT@cluster0.0eh4pca.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0eh4pca.mongodb.net/?appName=Cluster0`;
+
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -22,9 +24,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
-        const db = client.db("PawMart");
+        
+        const db = client.db(process.env.DB_NAME);
         const listingCollection = db.collection("listing");
         const ordersCollection = db.collection("orders");
 
@@ -33,20 +36,22 @@ async function run() {
             const result = await listingCollection.insertOne(newProduct);
             res.send(result);
         })
-                app.patch("/listing/:id", async (req, res) => {
-            const updateData = req.query;
+        app.patch("/listing/:id", async (req, res) => {
+            console.log("hit the patch");
+            const updateData = req.body;
             const id = req.params;
-            const query = { _id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: updateData
             }
             const result = await listingCollection.updateOne(query, updateDoc);
             res.send(result);
+            console.log("finish the patch");
         })
         app.delete("/listing/:id", async (req, res) => {
             const { id } = req.params;
             console.log(id);
-            const query = { _id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await listingCollection.deleteOne(query);
             res.send(result);
         })
@@ -94,7 +99,7 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
@@ -103,7 +108,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Backend server is now running!")
+    res.send("Backend server is now working!")
 })
 
 app.listen(port, () => {
